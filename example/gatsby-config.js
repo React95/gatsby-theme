e44f6785e59c080acc58e4530b7bf6c76b4c0865
ggdaltoso <ggdaltoso@gmail.com>
2023-04-22 18:10:44 -0300

    fix: remove `basePath` isn't necessary anymore

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,10 +1,14 @@
 module.exports = {
+  siteMetadata: {
+    title: 'React95 + Gatsby theme',
+    description: 'React95 Gatsby theme documentation',
+    siteUrl: 'https://react95.github.io/gatsby-theme',
+  },
   plugins: [
     {
       resolve: '@react95/gatsby-theme',
       options: {
         contentPath: 'content',
-        basePath: '/gatsby-theme',
       },
     },
   ],
